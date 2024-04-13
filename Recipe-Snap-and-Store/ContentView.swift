9cf1c2b3eec4fc60e19316abb740fb4c44f54413
werbowym-sheridan <werbowym@sheridancollege.ca>
2024-04-13 15:51:44 -0400

    Initial Commit

---
+++ Recipe-Snap-and-Store/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Recipe-Snap-and-Store
+//
+//  Created by Michael Werbowy on 2024-04-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Recipe-Snap-and-Store/Recipe_Snap_and_StoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_Snap_and_StoreApp.swift
+//  Recipe-Snap-and-Store
+//
+//  Created by Michael Werbowy on 2024-04-13.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_Snap_and_StoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
